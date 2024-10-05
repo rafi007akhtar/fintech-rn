97cbec19ae91cb341acd14e3d838c2bb3d35ef74
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-10-05 14:49:09 +0530

    made structural changes for initial layout and installed packages for video playback

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "react-native-reanimated/plugin", // NOTE: this has to be the LAST item in this array to work
+    ],
   };
 };
