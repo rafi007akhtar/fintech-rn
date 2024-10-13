b8518289f61c4f048ef3fc5f7b0ffde372900331
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-10-13 14:28:03 +0530

    using the RoundBtn component in the Home tab component

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,10 +1,62 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet, Button } from "react-native";
 import React from "react";
+import Colors from "../../../constants/Colors";
+import RoundBtn from "../../../components/RoundBtn";
+
+const tempBalc = 100;
 
 export default function Home() {
+  function onAddMoney() {}
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.currency}>₹</Text>
+          <Text style={styles.balance}>{tempBalc}</Text>
+        </View>
+      </View>
+
+      <View style={styles.actionRow}>
+        <RoundBtn icon="add" onPress={onAddMoney}>
+          Add Money
+        </RoundBtn>
+        <RoundBtn icon="refresh">Exchange</RoundBtn>
+        <RoundBtn icon="list">Details</RoundBtn>
+
+        {/* TODO: turn this into a dropdown */}
+        <RoundBtn icon="add">More</RoundBtn>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.background,
+  },
+  account: {
+    margin: 80,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  balance: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  actionRow: {
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
