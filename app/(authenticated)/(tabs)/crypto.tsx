2d89910df5302945f17c15cb45185c446f032f87
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-10-20 16:20:39 +0530

    getting listings from its api route and logging it in the crypto route page

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,7 +1,15 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Crypto() {
+  useEffect(() => {
+    async function getListings() {
+      const response = await fetch("/api/listings");
+      const result = await response.json();
+      console.log("result:", result);
+    }
+    getListings();
+  }, []);
   return (
     <View>
       <Text>Crypto</Text>
