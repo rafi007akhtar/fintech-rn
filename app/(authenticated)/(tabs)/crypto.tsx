0ffa1deb380f2de405ada634fd8bf372da6f1dcf
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-10-20 17:14:44 +0530

    replaced fetching of crypto info through api routes from useEffect to using tanstack useQuery hook, and showing the images alongwith the currency name

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,18 +1,46 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Currency } from "../../../models/crypto.model";
 
 export default function Crypto() {
-  useEffect(() => {
-    async function getListings() {
-      const response = await fetch("/api/listings");
-      const result = await response.json();
-      console.log("result:", result);
+  const currencies = useQuery({
+    queryKey: ["listings"],
+    queryFn: () => fetch("/api/listings").then((res) => res.json()),
+  });
+
+  const ids = currencies.data
+    ?.map((currency: Currency) => currency.id)
+    .join(",");
+
+  const { data } = useQuery({
+    queryKey: ["info"],
+    queryFn: () => fetch(`/api/info?id=${ids}`).then((res) => res.json()),
+    enabled: !!ids,
+  });
+
+  const currencyJSX = currencies.data?.map((currency: Currency) => {
+    let logo;
+    if (data) {
+      logo = data[currency.id].logo;
     }
-    getListings();
-  }, []);
-  return (
-    <View>
-      <Text>Crypto</Text>
-    </View>
-  );
+    return (
+      <View key={currency.id} style={styles.container}>
+        {logo && <Image source={{ uri: logo }} style={[styles.logo]} />}
+        <Text>{currency.name}</Text>
+      </View>
+    );
+  });
+
+  return <View>{currencyJSX}</View>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  logo: {
+    width: 32,
+    height: 32,
+  },
+});
