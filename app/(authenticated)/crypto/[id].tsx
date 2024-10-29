008a99f0008cb5fc9b2eea348cb0d6e78aa20b9c
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-10-29 23:02:16 +0530

    implemented victory native charts, with haptics, its own font, tooltip animated props using the tickers api route on react query's hook, for each of the crypto page

--- app/(authenticated)/crypto/[id].tsx
+++ app/(authenticated)/crypto/[id].tsx
@@ -7,23 +7,67 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { useHeaderHeight } from "@react-navigation/elements";
 import Colors from "../../../constants/Colors";
 import { useQuery } from "@tanstack/react-query";
 import { defaultStyles } from "../../../constants/Styles";
+import { CartesianChart, Line, useChartPressState } from "victory-native";
+import { Circle, useFont } from "@shopify/react-native-skia";
+import { format } from "date-fns/format";
+import * as Haptics from "expo-haptics";
+import Animated, {
+  SharedValue,
+  useAnimatedProps,
+} from "react-native-reanimated";
+import { TextInput } from "react-native-gesture-handler";
 
 const categories = ["Overview", "News", "Orders", "Transactions"];
+
+const AnimatedInput = Animated.createAnimatedComponent(TextInput);
+
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+  return <Circle cx={x} cy={y} r={8} color={Colors.primary} />;
+}
 
 export default function CryptoDetails() {
   const { id } = useLocalSearchParams();
   const headerHeight = useHeaderHeight();
   const [activeInd, setActiveInd] = useState(0);
+  const font = useFont(require("@/assets/fonts/SpaceMono-Regular.ttf"), 12);
+  const { state, isActive } = useChartPressState({ x: 0, y: { price: 0 } });
+
+  useEffect(() => {
+    if (isActive) {
+      Haptics.selectionAsync();
+    }
+  }, [isActive]);
 
   let { data } = useQuery({
     queryKey: ["info", id],
     queryFn: () => fetch(`/api/info?id=${id}`).then((res) => res.json()),
+  });
+
+  let { data: tickers } = useQuery({
+    queryKey: ["tickers", id],
+    queryFn: (): Promise<any[]> =>
+      fetch(`/api/tickers`).then((res) => res.json()),
+  });
+
+  const animatedPriceText = useAnimatedProps(() => {
+    return {
+      text: `₹ ${state.y.price.value.value.toFixed(2)}`,
+      defaultValue: "",
+    };
+  });
+
+  const animatedDateText = useAnimatedProps(() => {
+    const date = new Date(state.x.value.value);
+    return {
+      text: date.toLocaleDateString(),
+      defaultValue: "",
+    };
   });
 
   let elemToShow;
@@ -68,10 +112,72 @@ export default function CryptoDetails() {
           )}
           keyExtractor={(item, ind) => ind.toString()}
           renderItem={(item) => (
-            <View style={[defaultStyles.block, styles.sectionListItem]}>
-              <Text style={styles.subtitle}>Overview</Text>
-              <Text style={styles.sectionListPara}>{data.description}</Text>
-            </View>
+            <>
+              <View style={[defaultStyles.block, { height: 500 }]}>
+                {tickers && (
+                  <>
+                    {!isActive && (
+                      <View>
+                        <Text style={styles.todaysPrice}>
+                          ₹ {tickers[tickers.length - 1].price.toFixed(2)}
+                        </Text>
+                        <Text style={styles.todayText}>Today</Text>
+                      </View>
+                    )}
+                    {isActive && (
+                      <View>
+                        <AnimatedInput
+                          editable={false}
+                          underlineColorAndroid="transparent"
+                          animatedProps={animatedPriceText}
+                          style={styles.todaysPrice}
+                        ></AnimatedInput>
+                        <AnimatedInput
+                          editable={false}
+                          underlineColorAndroid="transparent"
+                          animatedProps={animatedDateText}
+                          style={styles.todayText}
+                        ></AnimatedInput>
+                      </View>
+                    )}
+                    <CartesianChart
+                      data={tickers}
+                      xKey="timestamp"
+                      yKeys={["price"]}
+                      chartPressState={state}
+                      axisOptions={{
+                        font,
+                        tickCount: 5,
+                        labelOffset: { x: -2, y: 0 },
+                        labelColor: Colors.gray,
+                        formatYLabel: (label) => `₹ ${label}`,
+                        formatXLabel: (ms) => format(new Date(ms), "MM/yy"),
+                      }}
+                    >
+                      {({ points }) => (
+                        <>
+                          <Line
+                            points={points.price}
+                            color={Colors.primary}
+                            strokeWidth={3}
+                          />
+                          {isActive && (
+                            <ToolTip
+                              x={state.x.position}
+                              y={state.y.price.position}
+                            />
+                          )}
+                        </>
+                      )}
+                    </CartesianChart>
+                  </>
+                )}
+              </View>
+              <View style={[defaultStyles.block, styles.sectionListItem]}>
+                <Text style={styles.subtitle}>Overview</Text>
+                <Text style={styles.sectionListPara}>{data.description}</Text>
+              </View>
+            </>
           )}
           style={styles.sectionList}
           ListHeaderComponent={() => (
@@ -148,5 +254,14 @@ const styles = StyleSheet.create({
   logo: {
     width: 60,
     height: 60,
+  },
+  todaysPrice: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: Colors.dark,
+  },
+  todayText: {
+    fontSize: 18,
+    color: Colors.gray,
   },
 });
